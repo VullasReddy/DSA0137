5ec17bbdaeb98c7afa7a82cf2c56e547d0adeaa0
VullasReddy <110800685+VullasReddy@users.noreply.github.com>
2022-09-07 10:27:00 +0530

    Add files via upload

---
+++ if else.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;
+	x=88;
+	y=99;
+	if(x > y)
+	{
+		cout << " x is greater";
+	}
+	else 
+	{
+		cout << " y is greater";
+	}
+	return 0;
+}
